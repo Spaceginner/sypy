0bd9c4f37b31ef103a7c7b9446a72adca86836c3
Spaceginner <ivan.demian2009@gmail.com>
2023-10-26 19:10:52 +0300

    make use of decorators in example

--- example.py
+++ example.py
@@ -1,25 +1,21 @@
 from typing import Annotated
 
 from sypy import Server
-from sypy.http import HTTPMethod
-from sypy.http.path import Path
 from sypy.parameters import Body
 
 
 server = Server()
 
 
+@server.post('/echo')
 def echo(stuff: Annotated[str, Body]) -> str:
     return stuff
 
 
+@server.get('/faulty')
 def faulty_callback() -> None:
     0 / 0
 
 
-server.dispatcher.register_callback(Path('/echo'), HTTPMethod.POST, echo)
-server.dispatcher.register_callback(Path('/faulty'), HTTPMethod.GET, faulty_callback)
-
-
 if __name__ == '__main__':
-    server.start(3001, listen=True, workers=4)
+    server.start(3000, listen=True)
