fcd2c94c55a90eb3b7d9e18ab1a1343a4eab8bbb
Spaceginner <ivan.demian2009@gmail.com>
2023-10-26 19:10:16 +0300

    reimport `Path` in `sypy.http` too

--- sypy/http/__init__.py
+++ sypy/http/__init__.py
@@ -4,3 +4,4 @@ from ._frames import Headers, QueryParams, HTTPRequest, HTTPResponse
 from ._status import HTTPStatus
 from ._errors import HTTPException, InvalidHTTPPacket
 from ._method import HTTPMethod
+from .path import Path
