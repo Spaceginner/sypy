ac87ee9aa4593472c0b1e3ef8aa02d1667129e81
Spaceginner <ivan.demian2009@gmail.com>
2023-10-28 20:35:47 +0300

    add an example endpoint showcasing injection, header, query params and a default value

--- example.py
+++ example.py
@@ -1,8 +1,8 @@
+import math
 from typing import Annotated
 
 from sypy import Server
-from sypy.parameters import Body
-
+from sypy.parameters import Body, Depends, Header
 
 server = Server()
 
@@ -17,5 +17,21 @@ def faulty_callback() -> None:
     0 / 0
 
 
+THE_EASTER_EGG_VALUE = "fastapi"
+
+
+def checker(the_thing: Annotated[str, Header]) -> bool:
+    return the_thing == THE_EASTER_EGG_VALUE
+
+
+def circle_area(radius: int = 4) -> float:
+    return math.pi * radius*radius
+
+
+@server.get('/is_it')
+def jection(is_it: Annotated[bool, Depends(checker)], area: Annotated[float, Depends(circle_area)]) -> str:
+    return f"{"yes, it is actually it" if is_it else "my condolences, it is not"}; btw it is {area:.2f}"
+
+
 if __name__ == '__main__':
-    server.start(3000, listen=True)
+    server.start(3001)
