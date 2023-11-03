d2143846b7bfd4a0335a5e670e2ac494eb44e651
spaceginner <ivan.demian2009@gmail.com>
2023-11-03 22:10:17 +0200

    add headers in http exceptions examples yes i did feel cruel with the loop one, also jesus christ it takes long for firefox to figure out it is a trap ...what if my text actually works? i dont know how chrome behaves tho

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import math
 from typing import Annotated
 
 from sypy import Server, RunConfig
+from sypy.http import HTTPException, HTTPStatus, Headers
 from sypy.parameters import Body, Depends, Header
 
 server = Server()
@@ -31,6 +32,37 @@ def circle_area(radius: int = 4) -> float:
 @server.get('/is_it')
 def jection(is_it: Annotated[bool, Depends(checker)], area: Annotated[float, Depends(circle_area)]) -> str:
     return f"{"yes, it is actually it" if is_it else "my condolences, it is not"}; btw it is {area:.2f}"
+
+
+LOCATIONS = {
+    'google': "https://www.google.com/",
+    'youtube': "https://www.youtube.com/",
+    'arcos': "https://www.izk-arcos.nl/",
+}
+
+
+@server.get('/redirect')
+def headers(where: str) -> None:
+    try:
+        raise HTTPException(
+            HTTPStatus.PermanentRedirect,
+            "follow that way, traveller", 
+            Headers(location=LOCATIONS[where])
+        )
+    except KeyError:
+        raise HTTPException(
+            HTTPStatus.NotFound,
+            "the place is unkown for me, traveller"
+        ) from None
+
+
+@server.get('/redirect/loop')
+def looping() -> None:
+    raise HTTPException(
+        HTTPStatus.MovedPermanently,
+        "this is definetely not a trap",
+        Headers(location='/redirect/loop')
+    )
 
 
 if __name__ == '__main__':
