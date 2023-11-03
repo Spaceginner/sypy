64aee7023aab2af42317fcdee45d7a83c457fae5
spaceginner <ivan.demian2009@gmail.com>
2023-11-03 21:55:16 +0200

    return the path overload, just in a different way

--- sypy/http/path/__init__.py
+++ sypy/http/path/__init__.py
@@ -10,17 +10,21 @@ class Path:
 
     @staticmethod
     def _validate_path(path: str) -> None:
-        if not path.startswith('/'):
-            raise ValueError(f"invalid path, doesn't begin with '/'")
+        # TODO add check for `/` in the beginning
 
         for c in path:
             if c not in RESERVED + UNRESERVED + '%':
                 raise ValueError(f"invalid path, unexpected char: {c}")
 
-    def __init__(self, path: str) -> None:
-        self._validate_path(path)
+    def __init__(self, path: str | list[str]) -> None:
+        path_parts = path.removesuffix('/').removeprefix('/').split('/') if isinstance(path, str) else path
+
+        # TODO combine 2 for-loops
+        for part in path_parts:
+            self._validate_path(part)
+
         # TODO remove the inner if-statement at all (ie move the check out)
-        self.parts = [part if '%' not in part else decode(part) for part in path.removesuffix('/').removeprefix('/').split('/')]
+        self.parts = [part if '%' not in part else decode(part) for part in path_parts]
 
     def __str__(self) -> str:
         return f"/{'/'.join(self.parts)}"
